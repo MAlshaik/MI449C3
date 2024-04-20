dd2f3e292fe4007d52ed2a7d82ec8c9c636881ec
MAlshaik <alshaik8@msu.edu>
2024-04-20 17:45:04 -0400

    init

---
+++ miapp/app/page.js
@@ -0,0 +1,69 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex flex-row items-center justify-center">
+        <h1 className="text-4xl font-bold">Area 51</h1>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">1</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">2</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">3</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">4</h1>
+        </div>
+
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <h1 className="text-4xl font-bold">Area 91</h1>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">1</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">2</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">3</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">4</h1>
+        </div>
+
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <h1 className="text-4xl font-bold">Area 93</h1>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">1</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">2</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">3</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+            <Image src="/flask.webp" alt="Alien" width={200} height={200} />
+            <h1 className="text-4xl font-bold">4</h1>
+        </div>
+
+      </div>
+
+   </main>
+  );
+}
